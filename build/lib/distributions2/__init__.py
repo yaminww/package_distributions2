af0e94a30c2c3b6a2eb65ca96743aedf522986ee
hiudhw <a201223@outlook.com>
2023-05-19 20:41:22 -0700

    initialization

---
+++ build/lib/distributions2/__init__.py
@@ -0,0 +1,5 @@
+from distributions2.Gaussiandistribution import Gaussian
+
+# TODO: import the Binomial class from the Binomialdistribution module
+
+from distributions2.Binomialdistribution import Binomial
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions2',
+      version='1.1',
+      description='Gaussian distributions2',
+      packages=['distributions2'],
+      zip_safe=False)
